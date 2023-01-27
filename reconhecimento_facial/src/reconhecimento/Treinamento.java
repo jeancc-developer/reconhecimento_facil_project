5fb4f9939597d3f76f28fa18936c03c9240ff668
Jean Carlos <73586340+jeancc-developer@users.noreply.github.com>
2023-01-27 14:30:51 -0300

    add treinamento

---
+++ reconhecimento_facial/src/reconhecimento/Treinamento.java
@@ -0,0 +1,54 @@
+package reconhecimento;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.nio.IntBuffer;
+
+import org.bytedeco.opencv.opencv_core.*;
+import org.bytedeco.opencv.opencv_face.EigenFaceRecognizer;
+import org.bytedeco.opencv.opencv_face.FaceRecognizer;
+
+import static org.bytedeco.opencv.global.opencv_core.*;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.*;
+import static org.bytedeco.opencv.global.opencv_imgproc.resize;
+
+public class Treinamento {
+	public static void main(String[] args) {
+		File diretorio = new File("src\\fotos");
+		FilenameFilter filtroImagem = new FilenameFilter() {
+
+			@Override
+			public boolean accept(File dir, String nome) {
+				return nome.endsWith(".jpg") || nome.endsWith(".gif") || nome.endsWith(".png");
+			}
+		};
+
+		File[] arquivos = diretorio.listFiles(filtroImagem);
+		MatVector fotos = new MatVector(arquivos.length);
+		Mat rotulos = new Mat(arquivos.length, 1, CV_32SC1);
+		IntBuffer rotulosBuffer = rotulos.createBuffer();
+		int contador = 0;
+		for (File imagem : arquivos) {
+			Mat foto = imread(imagem.getAbsolutePath(), IMREAD_GRAYSCALE);
+			int classe = Integer.parseInt(imagem.getName().split("\\.")[1]);
+			// System.out.println(classe);
+			resize(foto, foto, new Size(160, 160));
+			fotos.put(contador, foto);
+			rotulosBuffer.put(contador, classe);
+			contador++;
+		}
+
+		FaceRecognizer eigenFaces = EigenFaceRecognizer.create();
+		FaceRecognizer fisherFaces = EigenFaceRecognizer.create();
+		FaceRecognizer lbph = EigenFaceRecognizer.create();
+
+		eigenFaces.train(fotos, rotulos);
+		eigenFaces.save("src\\recursos\\classificarEigenFaces.yml");
+		fisherFaces.train(fotos, rotulos);
+		fisherFaces.save("src\\recursos\\classificadorFisherFaces.yml");
+		lbph.train(fotos, rotulos);
+		lbph.save("src\\recursos\\classificadorLBPH.yml");
+
+	}
+
+}
